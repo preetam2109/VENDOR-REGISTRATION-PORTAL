8ea6fb17a8137295b9f363df73f1b8ee90369cc3
preetam2109 <preetamlahre2109@gmail.com>
2025-11-12 11:37:49 +0530

    asdsdsds

---
+++ src/app/component/VENDER_REG/manufacturing-tabs-under/manufacturing-tabs-under.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manufacturing-tabs-under',
+  imports: [],
+  templateUrl: './manufacturing-tabs-under.html',
+  styleUrl: './manufacturing-tabs-under.css'
+})
+export class ManufacturingTabsUnder {
+
+}
